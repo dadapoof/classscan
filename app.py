791d03db4c102e34be969580580e252f34331144
David Massart <32443128+dadapoof@users.noreply.github.com>
2018-05-04 17:17:53 -0400

    changes

--- app.py
+++ app.py
@@ -1,87 +1,57 @@
-from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
-from data import Students
-from flask_mysqldb import MySQL
-from wtforms import Form, StringField, TextAreaField, PasswordField, validators
-from passlib.hash import sha256_crypt
+# We will be using Python's OpenCv2 Library to build our Face Scan API
 
-app = Flask(__name__)
+import cv2
+import numpy as np
+from PIL import ImageGrab
 
-# Configuration of MySQL
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'dadapoof$'
-app.config['MYSQL_DB'] = 'classscan'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+# Getting the OpenCV2 Classifier to detect the frontal face of people
+face_csc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-# Initializing MYSQL
-mysql = MySQL(app)
+# Getting access to my computer's webcam
+camera = cv2.VideoCapture(0)
 
-Students = Students()
+# Getting user input for user id!
+id = raw_input('enter user id')
 
-@app.route('/')
-def index():
-	return render_template('index.html')
+# A counter that will be used later!
+counter = 0;
 
-@app.route('/index.html')
-def home():
-	return render_template('index.html')
+# Infinite Loop 
+while(True):
+	# Gets images from my webcam
+	ret, img = camera.read()
 
-@app.route('/login.html')
-def login():
-	return render_template('login.html')
+	# Changes the images captured from colored to gray
+	# This helps in terms of facial recognition
+	change_to_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-@app.route('/administrator.html')
-def students():
-	return render_template('administrator.html' students = Students)
+	# Detects objects in the picture and returns a list of rectangles
+	faces = face_csc.detectMultiScale(change_to_gray, 1.3, 5)
 
-@app.route('/administrator/<string:id>/')
-def student(id):
-	return render_template('administrator.html', id=id)
+	# Basically looking at the rectangles returned by the detectMultiScale function
+	for (x, y, w, h) in faces:
+		# Increasing our counter
+		counter = counter+1;
 
-class RegisterForm(Form):
-	school_name = StringField('School', [validators.Length(min=1, max=200)])
-	school_address = StringField('School Address', [validators.Length(min=5, max=250)])
-	school_phone = StringField('School Phone', [validators.Length(min=10, max=10)])
-	name = StringField('Name', [validators.Length(min=1, max=50)])
-	username = StringField('Username', [validators.Length(min=4, max=50)])
-	email = StringField('Email', [validators.Length(min=6, max=50)])
-	password = PasswordField('Password', [
-		validators.DataRequired(),
-		validators.EqualTo('confirm', message='Passwords do not match')
-		])
-	confirm = PasswordField('Confirm Password')
+		# Saving the pictures in a folder as a unique name
+		# This is why we have a counter! Each picture has a different id
+		cv2.imwrite("dataSet/User." + str(id) + "." + str(counter) + ".jpg", change_to_gray[y:y+h, x:x+w])
 
-@app.route('/ask.html', methods=['GET', 'POST'])
-def register():
-	form = RegisterForm(request.form)
-	if request.method == 'POST' and form.validate():
-		school_name = form.school_name.data
-		school_address = form.school_address.data
-		school_phone = form.school_phone.data
-		name = form.name.data
-		username = form.username.data
-		email = form.email.data
-		password = sha256_crypt.encrypt(str(form.password.data))
+		# Forming a rectangle for every face recognized
+		# We made the rectangle green
+		cv2.rectangle(img, (x,y), (x+w, y+h), (0, 255, 0), 2)
 
-		# Creating the cursor
-		# Allows connection to MYSQL
-		cur = mysql.connection.cursor()
+		cv2.waitKey(150)
 
-		# Execute Query
-		cur.execute("INSERT INTO users(school_name, school_address, school_phone, name, username, email, password) VALUES(%s, %s, %s, %s, %s, %s, %s)", (school_name, school_address, school_phone, name, username, email, password))
+	# 
+	cv2.imshow('img', img)
+	
+	cv2.waitKey(1)
 
-		# Commit to the DATABASE
-		mysql.connection.commit()
+	# Breaks out of the while loop after taking a bunch of pictures
+	if (counter > 30):
+		break
 
-		# Closing the connection
-		cur.close()
-
-		flash('Congrats! You are now registered to ClassScan and can log in!', 'success')
-
-		return redirect(url_for('login'))
-	return render_template('ask.html', form=form)
-
-
-if __name__ == '__main__':
-	app.secret_key='secret123'
-	app.run(debug=True)
+# Closing the camera and shutting down/closing the windows
+camera.release()
+cv2.destroyAllWindows()
